46618ff64d989a39830d9f412f7b7d0edde0e44c
Lucas <Lucas@imaginelearning.cn>
2017-12-29 19:37:40 +0800

    - 管理员查询我的绘本 - 教师查询我的绘本

--- Imagine.BookManager.Application/SetService/SetAppService.cs
+++ Imagine.BookManager.Application/SetService/SetAppService.cs
@@ -3,6 +3,7 @@ using Abp.UI;
 using Imagine.BookManager.Common;
 using Imagine.BookManager.Core.Entity;
 using Imagine.BookManager.Dto.Set;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,6 +17,10 @@ namespace Imagine.BookManager.SetService
         {
             _setRepostitory = setRepository;
         }
+
+        private readonly IRepository<Admin> AdminRepostitory;
+
+        private readonly IRepository<Set> SetRepostitory;
 
         public bool CheckSetName(string name)
         {
@@ -50,6 +55,95 @@ namespace Imagine.BookManager.SetService
         {
             var list = _setRepostitory.GetAllIncluding(x => x.Books).ToList();
             return ObjectMapper.Map<List<SetDto>>(list);
+        }
+
+        public PaginationDataList<SetDto> SearchPicBook(int? pageSize, int? pageRows, int setStatus, Guid userId)
+        {
+            var admin = AdminRepostitory.GetAllIncluding(e => e.Orders, e => e.TeacherAllocations).FirstOrDefault(e => e.UserId == userId);
+            if (admin.UserType == UserType.Admin)
+            {
+                return GetPicBooksByAdmin(pageSize, pageRows, setStatus, admin);
+            }
+            else
+            {
+                return GetPicBooksByTeacher(pageSize, pageRows, setStatus, admin);
+            }
+        }
+
+        private PaginationDataList<SetDto> GetPicBooksByAdmin(int? pageSize, int? pageRows, int setStatus, Admin admin)
+        {
+            List<SetDto> setDtoList = new List<SetDto>();
+            var orderItems = admin.Orders.SelectMany(e => e.OrderItems);
+            var payments = admin.Orders.SelectMany(e => e.Payments);
+            var books = orderItems.Select(e => e.Set).SelectMany(e => e.Books);
+            var teacherAllocations = orderItems.SelectMany(e => e.TeacherAllocations);
+            foreach (var orderItem in orderItems)
+            {
+                var allocatedNum = 0;
+                teacherAllocations
+                    .Where(e => e.OrderItemId == orderItem.Id).ToList()
+                    .ForEach(e => allocatedNum = allocatedNum + e.Credit);
+                if (FilterSetBySetStatus(setStatus, allocatedNum, orderItem.Quantity))
+                    continue;
+                SetDto setDto = new SetDto()
+                {
+                    Id = orderItem.SetId,
+                    Synopsis = orderItem.Set.Synopsis,
+                    SetName = orderItem.Set.SetName,
+                    SetType = HintInfo.StandardCourse,
+                    Books = books.Where(e => e.SetId == orderItem.SetId).ToList(),
+                    DateCreated = payments.FirstOrDefault(e => e.OrderRef == orderItem.OrderRef)?.DateCreated,
+                    SurplusCredit = allocatedNum + "/" + orderItem.Quantity
+                };
+                setDtoList.Add(setDto);
+            }
+            return setDtoList.OrderByDescending(e => e.DateCreated).AsQueryable().ToPagination(pageSize, pageRows);
+        }
+
+        private PaginationDataList<SetDto> GetPicBooksByTeacher(int? pageSize, int? pageRows, int setStatus, Admin admin)
+        {
+            List<SetDto> setDtoList = new List<SetDto>();
+            var studentAllocations = admin.TeacherAllocations.SelectMany(e => e.StudentAllocations);
+            foreach (var teacherAllocation in admin.TeacherAllocations)
+            {
+                int allocatedNum = studentAllocations.Count(e => e.TeacherAllocationId == teacherAllocation.Id);
+                if (FilterSetBySetStatus(setStatus, allocatedNum, teacherAllocation.Credit))
+                    continue;
+                var setInfo = SetRepostitory.GetAllIncluding(e => e.Books).FirstOrDefault(e => e.Id == teacherAllocation.SetId);
+                SetDto setDto = new SetDto()
+                {
+                    Id = teacherAllocation.SetId,
+                    SetName = setInfo.SetName,
+                    SetType = HintInfo.StandardCourse,
+                    Synopsis = setInfo.Synopsis,
+                    Books = setInfo.Books,
+                    DateCreated = teacherAllocation.DateAllocated,
+                    SurplusCredit = allocatedNum + "/" + teacherAllocation.Credit
+                };
+                setDtoList.Add(setDto);
+            }
+            return setDtoList.OrderByDescending(e => e.DateCreated).AsQueryable().ToPagination(pageSize, pageRows);
+        }
+
+        private bool FilterSetBySetStatus(int setStatus,int allocatedNum, int quantity)
+        {
+            switch (setStatus)
+            {
+                case 1:
+                    if (allocatedNum == 0)
+                        return true;
+                    return false;
+                case 2:
+                    if (allocatedNum > 0)
+                        return true;
+                    return false;
+                case 3:
+                    if (allocatedNum != quantity)
+                        return true;
+                    return false;
+                default:
+                    return false;
+            }
         }
     }
 }
